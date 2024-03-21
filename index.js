c88ef947022b9158956c126dafefef9c1b9e61f4
Kgothatso Maisa <123masiakgothatso@gmail.com>
2024-03-21 13:26:48 +0200

    animation and styling

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 
+let sections = document.querySelectorAll('section');
+
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
 
@@ -14,4 +16,19 @@ function show() {
 
 function close() {
     mainMenu.style.top = '-100%';
+}
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            sec.classList.add('show-animate');
+        }
+        else {
+            sec.classList.remove('show-animate');
+        }
+    })
 }
